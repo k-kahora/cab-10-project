740e74054fde8902ee21472bcf6856e6daa5e943
malcolm <malcolm@debian.malcolm.com>
2023-04-29 16:24:44 -0400

    Exteding script

--- project_DML_commands.sql
+++ project_DML_commands.sql
@@ -10,15 +10,5 @@ INSERT INTO charger_type VALUES (
 'Lucid Super Charger', True, False, False
 );
 
-INSERT INTO Charger VALUES (
-'Tesla Super Charger', '123 Example St.', 'Mercer', '08550', 2, 14.99
-);
-
-INSERT INTO Charger VALUES (
-'Rivian Super Charger', '123 Example Ave', 'Middlesex', '08520', 5, 20.99
-);
-
-INSERT INTO Charger VALUES (
-'Lucid Super Charger', '123 Example Dr.', 'Ocean', '08546', 1, 10.01
-);
+\i project_DML_charger
 
