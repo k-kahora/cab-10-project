1122d8c1f50c9f3a841fdefbd56cf7d2ab51e465
alexq <alexquezada10@gmail.com>
2023-04-30 18:30:51 -0400

    update readme and add docs

--- project_DML_commands.sql
+++ project_DML_commands.sql
@@ -1,14 +1,14 @@
-INSERT INTO charger_type VALUES (
-'Tesla Super Charger', False, False, True
-);
+-- INSERT INTO charger_type VALUES (
+-- 'Tesla Super Charger', False, False, True
+-- );
 
-INSERT INTO charger_type VALUES (
-'Rivian Super Charger', False, True, False
-);
+-- INSERT INTO charger_type VALUES (
+-- 'Rivian Super Charger', False, True, False
+-- );
 
-INSERT INTO charger_type VALUES (
-'Lucid Super Charger', True, False, False
-);
+-- INSERT INTO charger_type VALUES (
+-- 'Lucid Super Charger', True, False, False
+-- );
 
-\i project_DML_charger
+-- \i project_DML_charger
 
